a71719cd80e3f308f0193ab6d9e817211a8088e3
Agustin Scian Forte <agustinscianfortewk@gmail.com>
2024-08-03 16:37:06 -0300

    add custom Scroll Network

--- front-zup/src/index.js
+++ front-zup/src/index.js
@@ -5,24 +5,40 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThirdwebProvider, coinbaseWallet, embeddedWallet, metamaskWallet, phantomWallet, trustWallet, walletConnect } from "@thirdweb-dev/react";
 
+// Define your custom network configuration
+const customNetwork = {
+  chainId: 534352, 
+  rpc: ["https://534352.rpc.thirdweb.com/${THIRDWEB_API_KEY}"],
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "CTK",
+    decimals: 18,
+  },
+  blockExplorers: {
+    default: { name: "Custom Explorer", url: "https://scrollscan.com" },
+  },
+  name: "Scroll",
+};
+
+// Configure the ThirdwebProvider with the custom network and supported wallets
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThirdwebProvider
-      activeChain="ethereum"
-      clientId="b1d2187beccc295e57ae5922065821dd"
+      activeChain={customNetwork}
+      clientId="b1d2187beccc295e57ae5922065821dd" // Asegúrate de usar tu propio clientId aquí
       supportedWallets={[
         embeddedWallet(),
         metamaskWallet(),
         phantomWallet(),
         coinbaseWallet(),
         walletConnect(),
-        trustWallet()
+        trustWallet(),
       ]}
     >
       <App />    
     </ThirdwebProvider>
-
   </React.StrictMode>
 );
+
 reportWebVitals();

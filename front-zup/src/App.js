8a86127f410e315f0926db069706643772f8b8c0
Agustin Scian Forte <agustinscianfortewk@gmail.com>
2024-08-03 16:35:08 -0300

    fetch /check ok

--- front-zup/src/App.js
+++ front-zup/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import SecondPage from './SecondPage';
+import CheckComponent from './CheckComponent';
 
 import Home from './Home';
 import './App.css';
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/second" element={<SecondPage />} />
         </Routes>
+        <CheckComponent />
       </div>
     </Router>
   );
---
+++ front-zup/src/CheckComponent.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+function CheckComponent() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://zup-backend-507748619dec.herokuapp.com/check')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+}
+
+export default CheckComponent;

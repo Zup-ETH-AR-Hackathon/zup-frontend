3acb356a84ff2443fcad858da56f595daa203165
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-08-04 08:14:18 -0300

    header fix

--- src/Header.js
+++ src/Header.js
@@ -1,32 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { ConnectWallet } from '@thirdweb-dev/react';
 
-
 function Header() {
-  const [walletAddress] = useState('');
-
-
-
   return (
     <header className="header">
       <div className="header-left">
-        <Link to="/" className="enabled-link">Zup In</Link>
-        <Link to="/mypools" className="disabled-link">My Pools</Link>
+        <Link to="/" className="enabled-link">
+          Zup In
+        </Link>
+        <Link to="/mypools" className="disabled-link">
+          My Pools
+        </Link>
       </div>
       <div className="header-right">
-        {walletAddress ? (
-          <span className="wallet-address">Address:{walletAddress}</span>
-        ) : (
-          <div>
         <ConnectWallet
           modalSize="wide"
           theme="light"
           btnTitle="Connect Wallet"
           modalTitle="Connect your favorite wallet"
+          onConnect={wallet => {console.log('connected to', wallet);
+          }}
         />
-      </div>
-        )}
       </div>
     </header>
   );

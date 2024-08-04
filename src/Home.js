3d0112e1f73bf14fce80fa38fb5269c310f7b5e8
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-08-04 08:15:27 -0300

    home fix

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,14 @@ import './Home.css';
 import { ReactComponent as QUESTION } from './resources/question.svg';
 import './Header.css';
 
-
 function Home() {
   return (
     <div className="home">
       <div className="home-content">
-      <div className="middle">
-        <h2 className='little-title'>ZUP IN</h2>
-        <QUESTION style={{ marginLeft: '10px' }} /> 
-      </div>
+        <div className="middle">
+          <h2 className="little-title">ZUP IN</h2>
+          <QUESTION style={{ marginLeft: '10px' }} />
+        </div>
         <TokenPairSelector />
       </div>
     </div>
